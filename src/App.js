153431a1f188a675e8dc6a711288a4b27f79ecf8
Soumik Chaudhuri <soumik.chaudhuri2000@gmail.com>
2020-08-01 21:32:55 +0530

    components and basic styles added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import TodoList from './components/todos/TodoList';
+import AddTodo from './components/ui/AddTodo';
 
-function App() {
-	return <div className="App" />;
-}
+const App = () => {
+	const [ todos, setTodos ] = useState([ 'Pick up groceres', 'Code for 5 hours' ]);
+
+	return (
+		<div className="container">
+			<div className="todo-card">
+				<h1>This is a TodoList</h1>
+				<AddTodo />
+				<TodoList todos={todos} />
+			</div>
+		</div>
+	);
+};
 
 export default App;
---
+++ src/components/ui/AddTodo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AddTodo = () => {
+	return (
+		<div className="new-todo">
+			<input type="text" className="add-todo" />
+			<button className="btn">Add Todo</button>
+		</div>
+	);
+};
+
+export default AddTodo;
